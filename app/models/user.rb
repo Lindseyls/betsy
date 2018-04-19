fe727339c873c607a3404ea255ce5dace41fd78c
Brenda Rios <riosbrenda@gmail.com>
2018-04-19 15:41:11 -0700

    Comments for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+#has_many :products
+# validate username presence and uniqueness
+# validate email presence and uniqueness
+
+# self method to get the info from github auth_hash
+# user = new
+# user.provider = provider
+# user.uid = auth_hash['uid']
+# user.username = auth_hash['info']['username']
+# user.email = auth_hash['info']['email']
+
 end
