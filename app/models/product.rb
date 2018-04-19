02181a5aae1bb404b12d6fe944cfcac1157d489e
Jamila <J.cornick21@gmail.com>
2018-04-19 15:55:07 -0700

    comments for product mvc

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
+# validate product name and uniqueness
 
+# has_many...
+
+# belongs_to :category
+
+# will there be logic for anything here?
+
+# I could not think of any...
 
 end
