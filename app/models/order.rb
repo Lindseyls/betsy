aace2db2a09df54ca6c3e4518c0347429e4b7f04
Jamila <J.cornick21@gmail.com>
2018-04-19 21:20:16 -0700

    space

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,19 @@
 class Order < ApplicationRecord
 
+
+  #------ Unauthorized User
+  # Orders have many products, Product has many orders
+  # Intermediary table where OrderProduct contains foreign keys for product, and order, and quantity
+    # functionality should include the total cost of the order
+  # Orders have product name, product id, merchant id?
+
+  # order fulfillment view could include (accessible to authorized user):
+    -product info
+    -merchant info
+
+
+    #  end
+
 #------ Authorized User
 # order fulfillment view could include (accessible to authorized user):
   -product info
@@ -8,18 +22,3 @@ class Order < ApplicationRecord
 
 # link to the user
 # link to the item description page
-
-
-
-#------ Unauthorized User
-# Orders have many products, Product has many orders
-# Intermediary table where OrderProduct contains foreign keys for product, and order, and quantity
-  # functionality should include the total cost of the order
-# Orders have product name, product id, merchant id?
-
-# order fulfillment view could include (accessible to authorized user):
-  -product info
-  -merchant info  
-
-
-end
