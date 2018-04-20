8f24ae32247f9ca0febef56663d84136f0b4a0e3
Brenda Rios <riosbrenda@gmail.com>
2018-04-20 15:22:42 -0700

    Added views for users and tests for the model.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def render_date(date)
+    date.strftime("%b %e, %Y")
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  let(:user) {users(:one)}
+
+  it "Users#Account" do
+    login(user)
+    get user_account_path(user.id)
+    must_respond_with :success
+  end
+
+  describe "OrderFulfillment Page" do
+    let (:one) {users(:one)}
+
+    it "shows a user's order fulfillment page " do
+      login(one)
+
+      get user_orders_path(one.id)
+
+      must_respond_with :success
+    end
+
+    it "renders 404 if user does not exist" do
+      bogus_user_id = (User.last.id) + 1
+
+      get user_orders_path(bogus_user_id)
+      must_respond_with :not_found
+
+    end
+  end
 end
