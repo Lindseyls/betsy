d87d4f945d8697b2f445dc9208bcfa26d9847e5b
Brenda Rios <riosbrenda@gmail.com>
2018-04-21 12:00:41 -0700

    Created migrations to add columns for users model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420215210) do
+ActiveRecord::Schema.define(version: 20180421184734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(version: 20180420215210) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "email"
+    t.integer "uid", null: false
+    t.string "provider", null: false
   end
 
 end
