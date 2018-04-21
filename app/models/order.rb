b463f65b4f2476c554587651d7dbf18e6b7c42a4
Brenda Rios <riosbrenda@gmail.com>
2018-04-21 13:45:23 -0700

    Added validations to order model.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ApplicationRecord
 
+  STATUS = %w(pending paid complete cancelled)
+
+  has_many :order_items, dependent: :destroy
+
+  validates :status, presence: true, inclusion: { in: STATUS }
 end
