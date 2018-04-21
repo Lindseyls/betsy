ed4e97f1af3504fd2bea4ce86ac61aec70a757d3
emilcecarlisa <emilcecarlisa@gmail.com>
2018-04-21 15:01:34 -0700

    Added optional parameter to user column to products table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
-  validates :name, presence: true
+  # validates :name, presence: true
 
   has_many :order_items
   has_many :categories
   has_many :reviews, dependent: :destroy
-  belongs_to :users
+  belongs_to :users, optional: true
   has_and_belongs_to_many :categories
 
   # CATEGORIES = "home & bedding","health & grooming", "toys", "training & accessories", "food"
