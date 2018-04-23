aa161d1bc988b782cb2d77c370ff11f171689235
emilcecarlisa <emilcecarlisa@gmail.com>
2018-04-22 22:00:13 -0700

    About to reseed products csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,31 +16,31 @@ CSV.foreach(product_file, headers: true, header_converters: :symbol, converters:
 end
 
 
-user_file = Rails.root.join('db', 'user_seeds.csv')
-
-# might have to change to .open, and
-CSV.foreach(user_file, headers: true, header_converters: :symbol, converters: :all) do |row|
-  # data is in a Hash, create a product ising the row data?
-  data = Hash[row.headers.zip(row.fields)]
-  puts data
-  User.create!(data)
-  # we never do anything with the new data structure
-end
-
-category_file = Rails.root.join('db', 'categories.csv')
-
-# might have to change to .open, and
-CSV.foreach(category_file, headers: true, header_converters: :symbol, converters: :all) do |row|
-  # data is in a Hash, create a product ising the row data?
-  data = Hash[row.headers.zip(row.fields)]
-  puts data
-  Category.create!(data)
-  # we never do anything with the new data structure
-end
-
-review_file = Rails.root.join('db', 'review_seeds.csv')
-
-CSV.foreach(review_file, headers: true, header_converters: :symbol, converters: :all) do |row|
-  data = Hash[row.headers.zip(row.fields)]
-  Review.create!(data)
-end
+# user_file = Rails.root.join('db', 'user_seeds.csv')
+#
+# # might have to change to .open, and
+# CSV.foreach(user_file, headers: true, header_converters: :symbol, converters: :all) do |row|
+#   # data is in a Hash, create a product ising the row data?
+#   data = Hash[row.headers.zip(row.fields)]
+#   puts data
+#   User.create!(data)
+#   # we never do anything with the new data structure
+# end
+#
+# category_file = Rails.root.join('db', 'categories.csv')
+#
+# # might have to change to .open, and
+# CSV.foreach(category_file, headers: true, header_converters: :symbol, converters: :all) do |row|
+#   # data is in a Hash, create a product ising the row data?
+#   data = Hash[row.headers.zip(row.fields)]
+#   puts data
+#   Category.create!(data)
+#   # we never do anything with the new data structure
+# end
+#
+# review_file = Rails.root.join('db', 'review_seeds.csv')
+#
+# CSV.foreach(review_file, headers: true, header_converters: :symbol, converters: :all) do |row|
+#   data = Hash[row.headers.zip(row.fields)]
+#   Review.create!(data)
+# end
