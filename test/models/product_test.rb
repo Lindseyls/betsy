dcf830b1d4b6d17b61d99c6031f7816e23f3639d
Brenda Rios <riosbrenda@gmail.com>
2018-04-23 15:29:38 -0700

    Tests for product model started

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,8 +2,31 @@ require "test_helper"
 
 describe Product do
   let(:product) { Product.new }
+  let(:valid_product) {products(:valid_product)}
+  let(:one) {products(:one)}
+  let(:two) {users(:two)}
 
-  it "must be valid" do
-    value(product).must_be :valid?
+  describe 'relations' do
+    it "must belong to a user" do
+      valid_product.valid?.must_equal false
+      valid_product.errors.keys.must_include :user
+    end
+
   end
+  describe 'validations' do
+    it "must have a name to be valid" do
+      one.valid?.must_equal true
+      one.name = ""
+      one.valid?.must_equal false
+    end
+
+    it "must have a unique name" do
+      product = products(:two)
+      product.name = products(:one).name
+      product.valid?.must_equal false
+    end
+
+  end
+
+
 end
