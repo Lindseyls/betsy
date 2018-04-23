bfbc8872f56cccf8d87340c237fa2c677efdbdb3
Brenda Rios <riosbrenda@gmail.com>
2018-04-23 11:22:47 -0700

    Added a helper method to format prices.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
   def render_date(date)
     date.strftime("%b %e, %Y")
   end
+
+  def format_price(price)
+    if price.nil?
+      price = 0
+    end
+    "$" + format("%.2f", price)
+  end
 end
