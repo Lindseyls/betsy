3c343109d20bf95a7f292563f9d19febb27824c3
emilcecarlisa <emilcecarlisa@gmail.com>
2018-04-23 08:28:27 -0700

    About to rebase branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
-product_file = Rails.root.join('db', 'product_seeds.csv')
-
-# might have to change to .open, and
-CSV.foreach(product_file, headers: true, header_converters: :symbol, converters: :all) do |row|
-  # data is in a Hash, create a product ising the row data?
-  data = Hash[row.headers.zip(row.fields)]
-  puts data
-  Product.create!(data)
-  # we never do anything with the new data structure
-end
-
-
 user_file = Rails.root.join('db', 'user_seeds.csv')
 
 # might have to change to .open, and
@@ -24,6 +12,17 @@ CSV.foreach(user_file, headers: true, header_converters: :symbol, converters: :a
   data = Hash[row.headers.zip(row.fields)]
   puts data
   User.create!(data)
+  # we never do anything with the new data structure
+end
+
+product_file = Rails.root.join('db', 'product_seeds.csv')
+
+# might have to change to .open, and
+CSV.foreach(product_file, headers: true, header_converters: :symbol, converters: :all) do |row|
+  # data is in a Hash, create a product ising the row data?
+  data = Hash[row.headers.zip(row.fields)]
+  puts data
+  Product.create!(data)
   # we never do anything with the new data structure
 end
 
