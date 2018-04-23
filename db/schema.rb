78d098add9897c520de07c68e8c4d4d66382c54f
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-04-23 15:38:41 -0700

    Changed modem name for items_order back to order_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423221943) do
+ActiveRecord::Schema.define(version: 20180423223729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,14 +28,14 @@ ActiveRecord::Schema.define(version: 20180423221943) do
     t.index ["product_id"], name: "index_categories_products_on_product_id"
   end
 
-  create_table "items_order", force: :cascade do |t|
+  create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "order_id"
     t.bigint "product_id"
-    t.index ["order_id"], name: "index_items_order_on_order_id"
-    t.index ["product_id"], name: "index_items_order_on_product_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -80,8 +80,8 @@ ActiveRecord::Schema.define(version: 20180423221943) do
     t.string "provider", null: false
   end
 
-  add_foreign_key "items_order", "orders"
-  add_foreign_key "items_order", "products"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "products", "users"
   add_foreign_key "reviews", "products"
 end
