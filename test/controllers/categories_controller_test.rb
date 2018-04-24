0882bb494d308c618b68e9ce380c04a53322b359
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-04-24 10:07:26 -0700

    Updated testing for category controller and model

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,67 @@
 require "test_helper"
 
 describe CategoriesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'index' do
+    it 'sends a success response when there are many categories' do
+      Category.count.must_be :>, 0
+
+      # Act
+      get categories_path
+
+      # Assert
+      must_respond_with :success
+    end
+  end
+
+  describe 'new' do
+    it 'responds with success' do
+      # category = Category.first
+      get new_category_path
+
+      must_respond_with :success
+    end
+  end
+
+  describe "create" do
+    it "can add a valid category" do
+      # Arrange
+      category_data = {
+        name: 'test category name'
+      }
+      old_category_count = Category.count
+
+      # Assumption
+      Category.new(category_data).must_be :valid?
+
+      # Act
+      post categories_path, params: { category: category_data }
+
+      # # Assert
+      # # ***The HTTP response below
+      # must_respond_with :redirect
+      # must_redirect_to categorys_path
+
+      Category.count.must_equal old_category_count + 1
+      Category.last.name.must_equal category_data[:name]
+    end
+
+    it "won't add an invalid category" do
+      # Arrange
+      category_data = {
+        name: nil
+      }
+      old_category_count = Category.count
+
+      # Assumption
+      Category.new(category_data).wont_be :valid?
+
+      # Act
+      post categories_path, params: { category: category_data }
+
+      # Assert
+      # must_respond_with :bad_request
+      Category.count.must_equal old_category_count
+    end
+  end
+
 end
