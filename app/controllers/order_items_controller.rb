9ceddccbb9ab029a1e6f05d515350e1a07906420
Jamila <J.cornick21@gmail.com>
2018-04-24 15:49:00 -0700

    modified order_items controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,9 +1,31 @@
 class OrderItemsController < ApplicationController
 
+  def new
+    @order_item = OrderItem.new(order_id: params[:order_id])
+  end
+
+  def create
+    @order_item = OrderItem.new(order_item_params)
+
+    if @order_item.save
+      flash[:success] = "Product added successfully"
+      redirect_to order_item_path
+    else
+      flash.now[:failure] = "Didn't add to the cart"
+      render :new, status: :bad_request
+    end
+
+  end
+
   def update
     @order_item = OrderItem.find_by(id: params[:id])
 
     @order_item.update(id: params[:id], shipped: true)
     redirect_to user_path
+  end
+
+  private
+  def order_item_params
+    params.require(:order_item).permit(:quantity, :order_id, :product_id)
   end
 end
