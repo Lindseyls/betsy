b99b86cdb289a24b060691631eb49cd973bc6a56
Brenda Rios <riosbrenda@gmail.com>
2018-04-24 10:48:37 -0700

    Added tests to order model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,15 @@
 class Product < ApplicationRecord
-
-  validates :name, presence: true, uniqueness: true
-  validates :price, presence: true, numericality: {only_integer: true,  greater_than: 0}
-
-
+  
   has_many :order_items
   has_many :reviews, dependent: :destroy
   belongs_to :user, optional: true
   has_and_belongs_to_many :categories
+
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: {only_integer: true,  greater_than: 0}
+  validates :stock, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
 
   def show_rating
     @reviews = Review.all
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -2,8 +2,121 @@ require "test_helper"
 
 describe Order do
   let(:order) { Order.new }
+  let(:order1) { orders(:order_2) }#status complete
+  let(:order2) { orders(:order_1) }#staus pending
 
-  it "must be valid" do
-    value(order).must_be :valid?
+  describe "relations" do
+    it "has a list of orderitems" do
+      order1.order_items.each do |item|
+        item.must_be_instance_of OrderItem
+        item.order.must_be_instance_of Order
+        item.order.must_equal order1
+      end
+    end
   end
+
+  describe "validations" do
+    it "validates the presence of a status" do
+      order1.status = nil
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :status
+
+      order1.status = ""
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :status
+    end
+
+    it "has an email adress" do
+      order1.email = nil
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :email
+
+      order1.email = ""
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :email
+
+      order1.valid?.must_equal true
+    end
+
+    it "has an mailing address" do
+      order1.mail_address = nil
+      order.valid?.must_equal false
+      order1.errors.messages.must_include :mail_adress
+
+      order1.mail_adress = ""
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :mail_adress
+
+      order1.valid?.must_equal true
+    end
+
+    it "has a name for the credit card" do
+      order1.cc_name = nil
+      order.valid?.must_equal false
+      order1.errors.messages.must_include :cc_name
+
+      order1.cc_name = ""
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :cc_name
+
+      order1.cc_name "Dan"
+      order1.valid?.must_equal true
+    end
+
+    it "has a 16 digits in length number for the credit card" do
+      [782948281, 9876543219876543].each {|element|
+        o.credit_card = element
+        o.valid?.must_equal false
+      }
+    end
+
+    it "validation for credit card presence" do
+      order.cc_num= nil
+      order.valid?.must_equal false
+      order1.errors.messages.must_include :cc_num
+
+      order1.cc_num = ""
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :cc_num
+
+      order1.valid?.must_equal true
+    end
+
+    it "validation for cc_cvv presence" do
+      order.cc_cvv= nil
+      order.valid?.must_equal false
+      order1.errors.messages.must_include :cc_expiry
+
+      order1.cc_expiry = ""
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :cc_expiry
+
+      order1.valid?.must_equal true
+    end
+
+    it "validation for cc_cvv presence" do
+      order.cc_cvv= nil
+      order.valid?.must_equal false
+      order1.errors.messages.must_include :cc_cvv
+
+      order1.cc_cvv = ""
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :cc_cvv
+
+      order1.valid?.must_equal true
+    end
+
+    it "validation for billing_zip presence" do
+      order.billing_zip= nil
+      order.valid?.must_equal false
+      order1.errors.messages.must_include :billing_zip
+
+      order1.billing_zip = ""
+      order1.valid?.must_equal false
+      order1.errors.messages.must_include :billing_zip
+
+      order1.valid?.must_equal true
+    end
+  end
+
 end
