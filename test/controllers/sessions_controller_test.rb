06f9e75371c1283b4a5934b2cb50fd9277522033
Jamila <J.cornick21@gmail.com>
2018-04-24 09:11:37 -0700

    modified sessions test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,6 +3,23 @@ require "test_helper"
 describe SessionsController do
   describe 'auth_callback' do
     it "creates a new user" do
+      user = User.new(provider: 'github', uid: 77621, email: 'mail@me.org', name: 'Milkah Lamb')
+
+      user.must_be :valid?
+      original_count = User.count
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      # act
+      get auth_callback_path(:github)
+
+      # assert
+
+      must_redirect_to root_path
+      User.count.must_equal original_count + 1
+      session[:user_id].must_equal User.last.id
+
+
 
     end
 
