b82a74f28a81c9a0eab5e47c0ffc2d689d5d2555
Brenda Rios <riosbrenda@gmail.com>
2018-04-27 22:39:43 -0700

    fixed bug to add a review.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,42 +2,35 @@ class ReviewsController < ApplicationController
 
   def new
     @product = Product.find_by(id: params[:product_id])
+    @review = Review.new(product: @product)
+  end
+
+  def create
+    @product = Product.find_by(id: params[:product_id])
     if current_user && @user.id == @product.user.id
       flash[:status] = :failure
       flash[:result_text] = "You can't review your own product"
       redirect_to product_path(@product.id)
     else
-      render_404 unless @product
-      @review = Review.new(product: @product)
-    end
+      @review = Review.new(review_params)
+      @review.product = @product
 
-    def create
-      @product = Product.find_by(id: params[:product_id])
-      if current_user && @user.id == @product.user.id
-        flash[:status] = :failure
-        flash[:result_text] = "You can't review your own product"
-        redirect_to product_path(@product.id)
+      if @review.save
+        flash[:status] = :success
+        flash[:result_text] = "Successfully created review"
+        redirect_to product_path(@product)
       else
-        @review = Review.new(review_params)
-        @review.product = @product
-
-        if @review.save
-          flash[:status] = :success
-          flash[:result_text] = "Successfully created review"
-          redirect_to product_path(@product)
-        else
-          flash[:status] = :failure
-          flash[:result_text] = "Could not create review"
-          flash[:messages] = @review.errors.messages
-          render :new, status: :bad_request
-        end
+        flash[:status] = :failure
+        flash[:result_text] = "Could not create review"
+        flash[:messages] = @review.errors.messages
+        render :new, status: :bad_request
       end
     end
-
-    private
-    def review_params
-      params.require(:review).permit(:rating, :comments)
-    end
-
   end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :comments)
+  end
+
 end
