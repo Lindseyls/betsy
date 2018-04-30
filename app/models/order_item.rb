bf1db466572cc57328533f0f1f06a126fd44738b
Brenda Rios <riosbrenda@gmail.com>
2018-04-30 05:33:31 -0700

    refactor order item model.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,23 +3,22 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   validates :quantity, presence: true, numericality: {only_integer: true, greater_than: 0}
-  # validate :available?
-  # validate :available_quantity
+  validate :available?
+  validate :available_quantity
 
-  # def available?
-  #   product = self.product
-  #   if product.stock == 0
-  #     errors.add(:stock, "out of stock")
-  #   end
-  # end
+  def available?
+    product = self.product
+    if product.stock == 0
+      errors.add(:stock, "out of stock")
+    end
+  end
 
-  # def available_quantity
-  #   quantity = self.quantity
-  #   if quantity > self. product.stock
-  #     errors.add(:stock, "is #{self.product.stock}")
-  #   end
-
-  
+  def available_quantity
+    quantity = self.quantity
+    if quantity > self. product.stock
+      errors.add(:stock, "is #{self.product.stock}")
+    end
+  end
 
   def sub_total
     price = self.product.price
